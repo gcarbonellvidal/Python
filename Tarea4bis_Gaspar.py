d8edb0783fabf0d3923a7b4297a33059616797b7
gcarbonellvidal <gcarbonellvidal@gmail.com>
2024-09-25 14:53:00 +0200

    Ejercicios

---
+++ Tarea4bis_Gaspar.py
@@ -0,0 +1,9 @@
+Lengua = float(input("Inserte la nota de Lengua: "))
+Matematicas = float(input("Inserte la nota de Matematicas: "))
+Phyton = float(input("Inserte la nota de Programacion en Python: "))
+
+nota_media = (Lengua*0.34 + Matematicas*0.3 + Phyton*0.36)
+print("Lengua: " , Lengua)
+print("Matemáticas: " , Matematicas)
+print("Programación en Python: " , Phyton)
+print("Nota Final: " , nota_media) 
